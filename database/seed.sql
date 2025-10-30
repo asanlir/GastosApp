fbe23d70c18caef07bf5beaf177768927912d7de
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2025-10-30 22:12:02 +0100

    chore: remove internal/sensitive files from repo (keep locally via .gitignore)

--- database/seed.sql
+++
@@ -1,9 +0,0 @@
-USE economia_db;
-
--- Insertar categorías básicas
-INSERT IGNORE INTO categorias (nombre) VALUES
-    ('Alquiler'),
-    ('Facturas'),
-    ('Compra'),
-    ('Gasolina'),
-    ('Otros');
